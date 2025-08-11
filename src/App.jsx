4f399228b887007f0580ed13bfca5d8776a4609a
AhmedHesham <ahmed.hesham303303@gmail.com>
2025-08-11 11:37:18 +0300

    implementing routes using react-routerv6

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "mean",
+    element: <Menu />,
+  },
+]);
 function App() {
-  return <div>Ahmed</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
