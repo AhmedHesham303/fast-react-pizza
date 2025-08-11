8cadd1fe43f5c1267a7be69aa2e075145891ff74
AhmedHesham <ahmed.hesham303303@gmail.com>
2025-08-11 12:36:09 +0300

    building app layout

---
+++ src/ui/Header.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+function Header() {
+  return (
+    <header>
+      <Link>Fasr React Pizza Co.</Link>
+      <p>Ahmed</p>
+    </header>
+  );
+}
+
+export default Header;
