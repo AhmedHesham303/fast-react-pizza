70ece24dd96b8cfd6b4df5922ae8d2630df57adf
AhmedHesham <ahmed.hesham303303@gmail.com>
2025-08-11 15:22:09 +0300

    fixing a small error

--- src/features/order/SearchOrders.jsx
+++ src/features/order/SearchOrders.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useNavigate } from "react-router";
+import { useNavigate } from "react-router-dom";
 function SearchOrders() {
   const [query, setQuery] = useState("");
   const navigate = useNavigate();
